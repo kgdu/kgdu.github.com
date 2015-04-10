b3a55d57e210daac8e41649f2acf2803bb1dc93b
kgdu <184832985@qq.com>
2015-04-10 12:42:26 +0800

    test
    testphp

---
+++ test.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+
+<body>
+
+<h1>我的第一张 PHP 页面</h1>
+
+<?php
+echo "Hello World!";
+
+$url='http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid=570&format=json';  
+$html = file_get_contents($url);  
+echo $html;  
+
+?>
+
+
+</body>
+</html>
